7d1b546ce8c8581c9c8f88c0ee37cec4b5e56d96
cgy <100090733+cgyinthehouse@users.noreply.github.com>
2023-06-30 18:21:55 +0800

    fix: table borders

--- markdownPreviewer/src/components/ToggleTheme.tsx
+++ markdownPreviewer/src/components/ToggleTheme.tsx
@@ -8,8 +8,8 @@ const ThemeToggle:React.FC = () => {
       className={`hover:ring-yellow-200/50 hover:ring-2 transition-colors overflow-hidden h-6 w-6 space-y-[6px] text-xs relative left-[15%] md:left-[30%] rounded-full ${dark ? 'bg-neutral-800' : 'bg-neutral-500'}`}
       onClick={toggle}
     >
-      <span className={`${dark ? "" : "-translate-y-[22px]"}  left-[4px] top-[4px] absolute transition`}>🌙</span>
-      <span className={`${dark ? 'translate-y-[16px]' : '-translate-y-[6px]'}  left-[4px] top-[4px] absolute transition`}>🔆️</span>
+      <span className={`${dark ? "" : "-translate-y-[22px]"}  h-4 w-4 left-[4px] top-[4px] absolute transition`}>🌙</span>
+      <span className={`${dark ? 'translate-y-[16px]' : '-translate-y-[6px]'} h-4 w-4 left-[4px] top-[4px] absolute transition`}>🔆️</span>
     </button>
   );
 };
